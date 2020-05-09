edc31c85a150ac120268cf9ce1121c13bdad2ca3
Dylan <mugidylan@gmail.com>
2020-05-09 17:09:52 +0400

    display recipe infos

--- src/components/RecipesEpic/RecipesDetailsScreen/index.js
+++ src/components/RecipesEpic/RecipesDetailsScreen/index.js
@@ -1,11 +1,15 @@
 import React, { useEffect } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet, Image } from 'react-native';
 import { fetchSelectedRecipe } from '../../../api/recipes';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { getSelectedRecipe } from '../../../redux/selectors';
+
 
 const RecipesDetailsScreen = ({ route, navigation }) => {
     const { id } = route.params;
     const dispatch = useDispatch();
+
+    const recipe = useSelector(getSelectedRecipe);
 
     useEffect(() => {
         fetchSelectedRecipe(dispatch, id);
@@ -13,13 +17,41 @@ const RecipesDetailsScreen = ({ route, navigation }) => {
 
     return (
         <View>
-            <Text>RecipesDetailsScreen</Text>
-            <Text>{id}</Text>
-            <Button title='Retour' onPress={() => {
-                navigation.goBack();
-            }} />
+            <View style={styles.containerImage}>
+                <Image source={{ uri: recipe.image }} resizeMode='cover' style={styles.image}></Image>
+            </View>
+            <Text style={styles.title}>{recipe.title}</Text>
+            <View style={styles.containerIngredients}>
+                {recipe.extendedIngredients.map(ing => {
+                    return <Text style={styles.ing}>{ing.name}</Text>
+                })}
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    containerImage: {
+        height: 200
+    },
+    Image: {
+        height: 200
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 26,
+        textAlign: 'center',
+        marginVertical: 10
+    },
+    containerIngredients: {
+        borderTopWidth: 1,
+        borderTopColor: 'grey',
+        borderBottomColor: 'grey',
+        borderBottomWidth: 1
+    },
+    ing: {
+        fontSize: 16
+    }
+});
 
 export default RecipesDetailsScreen;
